5d13d7c0c4c80b1d16ba174f4723aeeff141c3cc
Izzy V <israelreyvarela@gmail.com>
2020-11-15 17:39:43 -0600

    updated code to deploy to gh pages

---
+++ build/precache-manifest.4bfe2f92e1e7ce49bc94d57708474d78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f34b3c425e95378e6daad252b23cbc6",
+    "url": "/Company-Employee-Directory/index.html"
+  },
+  {
+    "revision": "e9907e795a4d811cab1e",
+    "url": "/Company-Employee-Directory/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "ab46d118a68a902c122b",
+    "url": "/Company-Employee-Directory/static/css/main.8c316816.chunk.css"
+  },
+  {
+    "revision": "e9907e795a4d811cab1e",
+    "url": "/Company-Employee-Directory/static/js/2.0a6ff45d.chunk.js"
+  },
+  {
+    "revision": "ab46d118a68a902c122b",
+    "url": "/Company-Employee-Directory/static/js/main.22506c80.chunk.js"
+  },
+  {
+    "revision": "d1f98db18f993b0bdf2c",
+    "url": "/Company-Employee-Directory/static/js/runtime~main.2839d57f.js"
+  }
+]);
